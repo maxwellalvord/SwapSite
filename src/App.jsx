befc2e0228c0d83bd4de4e9a5e8df0c3f4612fad
Maxwell Alvord <maxwellalvord@gmail.com>
2022-11-16 19:43:54 -0800

    add react framework and initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+
+const app = () => {
+  return (
+   <>
+    <Header />
+    <Footer />
+   </>
+  )
+}
+
+export default app
